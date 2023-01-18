126f90d92fcc4fea53872b63df8cfd7c798a3c48
Arnold Lev <aalev@cpp.edu>
2023-01-17 18:22:34 -0800

    Updated

--- Board.java
+++ Board.java
@@ -73,7 +73,6 @@ public class Board {
     }
 
     public boolean AIMove(int i, int j) {
-        //int row = i - 65, col = j - 1;
         int row = i, col = j;
         if ( (row > 7 || row < 0) || (col > 7 || col < 0) || board[row][col] != '-' ) {
             return false;
@@ -85,114 +84,157 @@ public class Board {
     }
 
 
-
-    public int evaluate(char c) {
+    public int eval(char c) {
         char other = (c == 'X') ? 'O' : 'X';
-        int score2 = checkTwos(c) * 500, score3 = checkThrees(c) * 5000;
-        int other2 = checkTwos(other) * 500, other3 = checkThrees(other) * 5000;
-        return score2 + score3 - other2 - other3;
+        if (hasWon(c)) {
+            return 100000000;
+        }
+
+        if (hasWon(other)) {
+            return -100000000;
+        }
+
+        int score2 = scoreTwo(c) * 10, score3 = scoreThree(c) * 10000;
+        int other2 = scoreTwo(other) * 10, other3 = scoreThree(other) * 10000;
+        return score2 + score3 - other3 - other2;
     }
 
     public boolean isTerminalState() {
-        return (moves <= 0 || winner() != 0);
+        return (moves <= 0 || hasWon('O') || hasWon('X'));
     }
 
 
     public int scoreTwo(char c) {
         int score = 0;
-        for (int j = 0; j < 5; j++ ) {
+        for (int j = 0; j < 5; j++) {
             for (int i = 0; i < 8; i++) { 
-                if (   this.board[i][j] == c && this.board[i][j+1] == c && this.board[i][j+2] == '-' && this.board[i][j+3] == '-'
-                    || this.board[i][j] == c && this.board[i][j+1] == '-' && this.board[i][j+2] == '-' && this.board[i][j+3] == c
-                    || this.board[i][j] == '-' && this.board[i][j+1] == '-' && this.board[i][j+2] == c && this.board[i][j+3] == c 
-                    || this.board[i][j] == '-' && this.board[i][j+1] == c && this.board[i][j+2] == c && this.board[i][j+3] == '-') {
+                if ( (this.board[i][j] == c && this.board[i][j+1] == c && this.board[i][j+2] == '-' && this.board[i][j+3] == '-') ||
+                     (this.board[i][j] == c && this.board[i][j+1] == '-' && this.board[i][j+2] == '-' && this.board[i][j+3] == c) ||
+                     (this.board[i][j] == '-' && this.board[i][j+1] == '-' && this.board[i][j+2] == c && this.board[i][j+3] == c) ||
+                     (this.board[i][j] == '-' && this.board[i][j+1] == c && this.board[i][j+2] == c && this.board[i][j+3] == '-') ) 
+                {
+                    if (j > 1 && board[i][j-2] == '-' && board[i][j-1] == '-') {
+                        score += 2;
+                    } else {
                         score++;
                     }
+                }
+
+                if (j < 4 && (board[i][j+2] == c && board[i][j+3] == c && board[i][j] == '-' && board[i][j+1] == '-')) {
+                    score++;
+                }
+
+                if (j == 4) {
+                    if( (board[i][j+1] == c && board[i][j+2] == c && board[i][j] == '-' && board[i][j+3] == '-') ||
+                      (board[i][j+1] == c && board[i][j+3] == c && board[i][j] == '-' && board[i][j+2] == '-') ||
+                      (board[i][j+2] == c && board[i][j+3] == c && board[i][j] == '-' && board[i][j+1] == '-') ) 
+                    {
+                        score++;
+                    }
+                }
             }
         }
 
-        for (int i = 0; i< 5 ; i++ ){
-            for (int j = 0; j< 8; j++){
-                if (   this.board[i][j] == c && this.board[i+1][j] == c && this.board[i+2][j] == '-' && this.board[i+3][j] == '-'
-                    || this.board[i][j] == c && this.board[i+1][j] == '-' && this.board[i+2][j] == '-' && this.board[i+3][j] == c
-                    || this.board[i][j] == '-' && this.board[i+1][j] == '-' && this.board[i+2][j] == c && this.board[i+3][j] == c 
-                    || this.board[i][j] == '-' && this.board[i+1][j] == c && this.board[i+2][j] == c && this.board[i+3][j] == '-') {
+        for (int i = 0; i < 5 ; i++ ) {
+            for (int j = 0; j < 8; j++) {
+                if ( (this.board[i][j] == c && this.board[i+1][j] == c && this.board[i+2][j] == '-' && this.board[i+3][j] == '-') ||
+                     (this.board[i][j] == c && this.board[i+1][j] == '-' && this.board[i+2][j] == '-' && this.board[i+3][j] == c) ||
+                     (this.board[i][j] == '-' && this.board[i+1][j] == '-' && this.board[i+2][j] == c && this.board[i+3][j] == c) ||
+                     (this.board[i][j] == '-' && this.board[i+1][j] == c && this.board[i+2][j] == c && this.board[i+3][j] == '-') ) 
+                {
+                    if (i > 1 && board[i-2][j] == '-' && board[i-1][j] == '-') {
+                        score+=2;
+                    } else {
                         score++;
-                    }          
+                    }
+                }
+                
+
+                if (i < 4 && (board[i+2][j] == c && board[i+3][j] == c && board[i][j] == '-' && board[i+1][j] == '-')) {
+                    score++;
+                }
+                
+                if (i == 4) {
+                    if( (board[i+1][j] == c && board[i+2][j] == c && board[i][j] == '-' && board[i+3][j] == '-') ||
+                        (board[i+1][j] == c && board[i+3][j] == c && board[i][j] == '-' && board[i][j] == '-') ||
+                        (board[i+2][j] == c && board[i+3][j] == c && board[i][j] == '-' && board[i+1][j] == '-') )
+                    {
+                        score++;
+                    }
+                }
             }
         }
 
         return score;
     }
 
-    public int scoreThrees(char c) {
+    public int scoreThree(char c) {
         int score = 0;
         for (int j = 0; j < 5; j++ ) {
             for (int i = 0; i < 8; i++) { 
-                if (   this.board[i][j] == c && this.board[i][j+1] == c && this.board[i][j+2] == c && this.board[i][j+3] == '-'
-                    || this.board[i][j] == c && this.board[i][j+1] == c && this.board[i][j+2] == '-' && this.board[i][j+3] == c
-                    || this.board[i][j] == c && this.board[i][j+1] == '-' && this.board[i][j+2] == c && this.board[i][j+3] == c 
-                    || this.board[i][j] == '-' && this.board[i][j+1] == c && this.board[i][j+2] == c && this.board[i][j+3] == c) {
+                if ((this.board[i][j] == c && this.board[i][j+1] == c && this.board[i][j+2] == c && this.board[i][j+3] == '-') ||
+                    (this.board[i][j] == c && this.board[i][j+1] == c && this.board[i][j+2] == '-' && this.board[i][j+3] == c) ||
+                    (this.board[i][j] == c && this.board[i][j+1] == '-' && this.board[i][j+2] == c && this.board[i][j+3] == c) ||
+                    (this.board[i][j] == '-' && this.board[i][j+1] == c && this.board[i][j+2] == c && this.board[i][j+3] == c)) 
+                {
+                    if (j > 0 && board[i][j-1] == '-') {
+                        score += 2;
+                    } else {
                         score++;
                     }
+                }
+
+                if (j == 4 && (board[i][j+1] == c && board[i][j+2] == c && board[i][j+3] == c && board[i][j] == '-')) {
+                     score++;
+                }
             }
         }
 
-        for (int i = 0; i< 5 ; i++ ){
-            for (int j = 0; j< 8; j++){
-                if (   this.board[i][j] == c && this.board[i+1][j] == c && this.board[i+2][j] == c && this.board[i+3][j] == '-'
-                    || this.board[i][j] == c && this.board[i+1][j] == c && this.board[i+2][j] == '-' && this.board[i+3][j] == c
-                    || this.board[i][j] == c && this.board[i+1][j] == '-' && this.board[i+2][j] == c && this.board[i+3][j] == c 
-                    || this.board[i][j] == '-' && this.board[i+1][j] == c && this.board[i+2][j] == c && this.board[i+3][j] == c) {
+        for (int i = 0; i < 5 ; i++ ){
+            for (int j = 0; j < 8; j++){
+                if ((this.board[i][j] == c && this.board[i+1][j] == c && this.board[i+2][j] == c && this.board[i+3][j] == '-') ||
+                    (this.board[i][j] == c && this.board[i+1][j] == c && this.board[i+2][j] == '-' && this.board[i+3][j] == c) ||
+                    (this.board[i][j] == c && this.board[i+1][j] == '-' && this.board[i+2][j] == c && this.board[i+3][j] == c) ||
+                    (this.board[i][j] == '-' && this.board[i+1][j] == c && this.board[i+2][j] == c && this.board[i+3][j] == c)) 
+                {
+                    if (i > 0 && board[i-1][j] == '-') {
+                        score+=2;
+                    } else {
                         score++;
-                    }          
+                    }
+                }
+                
+                if (i == 4 && (board[i+1][j] == c && board[i+2][j] == c && board[i+3][j] == c && board[i][j] =='-')) {
+                    score++;
+                }
             }
         }
 
         return score;
     }
 
-    public int winner() {
-        // Check horizontal win for Human
+    public boolean hasWon(char c) {
+        // Check horizontal win 
         for (int j = 0; j<  5; j++ ){
             for (int i = 0; i < 8; i++){
-                if (this.board[i][j] == 'O' && this.board[i][j+1] == 'O' && this.board[i][j+2] == 'O' && this.board[i][j+3] == 'O'){
-                    return 500000;
+                if (this.board[i][j] == c && this.board[i][j+1] == c && this.board[i][j+2] == c && this.board[i][j+3] == c) {
+                    return true;
                 }           
             }
         }
        
-        // Check vertical win for Human
+        // Check vertical win
         for (int i = 0; i< 5 ; i++ ){
             for (int j = 0; j< 8; j++){
-                if (this.board[i][j] == 'O' && this.board[i+1][j] == 'O' && this.board[i+2][j] == 'O' && this.board[i+3][j] == 'O'){
-                    return 500000;
+                if (this.board[i][j] == c && this.board[i+1][j] == c && this.board[i+2][j] == c && this.board[i+3][j] == c) {
+                    return true;
                 }           
             }
         }
-
-        // Check horizontal win for Computer
-        for (int j = 0; j<  5; j++ ){
-            for (int i = 0; i < 8; i++){
-                if (this.board[i][j] == 'X' && this.board[i][j+1] == 'X' && this.board[i][j+2] == 'X' && this.board[i][j+3] == 'X'){
-                    return -500000;
-                }           
-            }
-        }
-
-        // Check vertical win for Computer
-        for (int i = 0; i< 5 ; i++ ){
-            for (int j = 0; j< 8; j++){
-                if (this.board[i][j] == 'X' && this.board[i+1][j] == 'X' && this.board[i+2][j] == 'X' && this.board[i+3][j] == 'X') {
-                    return -500000;
-                }           
-            }
-        }
-
-        return 0;
+        
+        return false;
     }
     
-
     public void printBoard() {
         System.out.println("  1 2 3 4 5 6 7 8");
         for (int i = 0; i < 8; i++) {
@@ -202,128 +244,6 @@ public class Board {
             }
             System.out.println();
         }
-    }
-
-
-    private int checkTwos(char c){
-        // System.out.println("Turn "+c);
-          int score = 0;
-          //check row
-          for(int col=0;col<5;col++){
-              for(int row=0;row<board.length;row++){
-                  if( (board[row][col]==c && board[row][col]==board[row][col+1] &&
-                       board[row][col+2]=='-' && board[row][col+3]=='-') ||
-                      (board[row][col]==c && board[row][col]==board[row][col+2] &&
-                       board[row][col+1]=='-' && board[row][col+3]=='-') ||
-                      (board[row][col]==c && board[row][col]==board[row][col+3] &&
-                       board[row][col+1]=='-' && board[row][col+2]=='-') ){
-                      if(col>1 && board[row][col-2]=='-' && board[row][col-1]=='-') score+=2;
-                      else score++;
-
-                  }
-                  if(col<4){
-                    if( (board[row][col+2]==c && board[row][col+2]==board[row][col+3] &&
-                         board[row][col]=='-' && board[row][col+1]=='-') ){
-                           score++;
-                      }
-                  }
-                  if(col==4){
-                      if( (board[row][col+1]==c && board[row][col+1]==board[row][col+2] &&
-                           board[row][col]=='-' && board[row][col+3]=='-') ||
-                          (board[row][col+1]==c && board[row][col+1]==board[row][col+3] &&
-                           board[row][col]=='-' && board[row][col+2]=='-') ||
-                          (board[row][col+2]==c && board[row][col+2]==board[row][col+3] &&
-                           board[row][col]=='-' && board[row][col+1]=='-') ){
-                             score++;
-                            // System.out.println(board[row][col]+" "+board[row][col+1]+" "+board[row][col+2]+" "+board[row][col+3]);
-                      }
-                  }
-              }
-          }
-        //check columns
-        for(int row=0;row<5;row++){
-            for(int col=0;col<board.length;col++){
-                if( (board[row][col]==c && board[row][col]==board[row+1][col] &&
-                     board[row+2][col]=='-' && board[row+3][col]=='-') ||
-                    (board[row][col]==c && board[row][col]==board[row+2][col] &&
-                     board[row+1][col]=='-' && board[row+3][col]=='-') ||
-                    (board[row][col]==c && board[row][col]==board[row+3][col] &&
-                     board[row+1][col]=='-' && board[row+2][col]=='-') ){
-                       // System.out.println(board[row][col]+" "+board[row+1][col]+" "+board[row+2][col]+" "+board[row+3][col]);
-                       if(row>1 && board[row-2][col]=='-' && board[row-1][col]=='-'){
-                         // System.out.println("YES "+board[row-2][col]+" "+board[row-1][col]+" "+board[row][col]+" "+board[row+1][col]+" "+board[row+2][col]+" "+board[row+3][col]);
-                         score+=2;
-                       }
-                       else score++;
-                }
-                if(row<4){
-                  if( (board[row+2][col]==c && board[row+2][col]==board[row+3][col] &&
-                       board[row][col]=='-' && board[row+1][col]=='-') ){
-                         score++;
-                    }
-                }
-                if(row==4){
-                    if( (board[row+1][col]==c && board[row+1][col]==board[row+2][col] &&
-                         board[row][col]=='-' && board[row+3][col]=='-') ||
-                        (board[row+1][col]==c && board[row+1][col]==board[row+3][col] &&
-                         board[row][col]=='-' && board[row][col]=='-') ||
-                        (board[row+2][col]==c && board[row+2][col]==board[row+3][col] &&
-                         board[row][col]=='-' && board[row+1][col]=='-') ){
-                           score++;
-                    }
-                }
-            }
-        }
-        // System.out.println(score+" twos");
-        return score;
-    }
-
-    private int checkThrees(char c){
-        int score = 0;
-        //check rows
-        for(int col=0;col<5;col++){
-            for(int row=0;row<board.length;row++){
-
-                if( (board[row][col]==c && board[row][col]==board[row][col+1] &&
-                     board[row][col+1]==board[row][col+2] && board[row][col+3]=='-') ||
-                    (board[row][col]==c && board[row][col]==board[row][col+1] &&
-                     board[row][col+1]==board[row][col+3] && board[row][col+2]=='-') ||
-                    (board[row][col]==c && board[row][col]==board[row][col+2] &&
-                     board[row][col+2]==board[row][col+3] && board[row][col+1]=='-') ){
-                         // System.out.println(board[row][col]+" "+board[row][col+1]+" "+board[row][col+2]+" "+board[row][col+3]);
-                    if(col>0 && board[row][col-1]=='-') score+=2;
-                    else score++;
-                }
-                if(col==4){
-                    if(board[row][col+1]==c && board[row][col+1]==board[row][col+2] &&
-                       board[row][col+2]==board[row][col+3] && board[row][col]=='-'){
-                         score++;
-                    }
-                }
-            }
-        }
-        //check columns
-        for(int row=0;row<5;row++){
-            for(int col=0;col<board.length;col++){
-                if( (board[row][col]==c && board[row][col]==board[row+1][col] &&
-                     board[row+1][col]==board[row+2][col] && board[row+3][col]=='-') ||
-                    (board[row][col]==c && board[row][col]==board[row+1][col] &&
-                     board[row+1][col]==board[row+3][col] && board[row+2][col]=='-') ||
-                    (board[row][col]==c && board[row][col]==board[row+2][col] &&
-                     board[row+2][col]==board[row+3][col] && board[row+1][col]=='-') ){
-                       if(row>0 && board[row-1][col]=='-') score+=2;
-                       else score++;
-                }
-                if(row==4){
-                    if(board[row+1][col]==c && board[row+1][col]==board[row+2][col] &&
-                       board[row+2][col]==board[row+3][col] && board[row][col]=='-'){
-                         score++;
-                    }
-                }
-            }
-        }
-        // System.out.println(score+" threes");
-        return score;
     }
 
 }
--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -8,7 +7,7 @@ public class Main {
         Board b = new Board('H');
         b.printBoard();
 
-        while (b.winner() == 0) {
+        while (!b.isTerminalState()) {
             if (b.getTurn() == 'H') {
                 System.out.println("Enter your move: (EX: A1)");
                 String in = scan.nextLine();
@@ -20,9 +19,9 @@ public class Main {
                 }
             } else {
                 int[] move = new int[2];
-                long start = System.currentTimeMillis();
+                long start = System.currentTimeMillis(); 
                 for (int i = 1; i <= 5 && (System.currentTimeMillis() - start) < 5000; i++) {
-                    //start = System.currentTimeMillis();
+                    start = System.nanoTime();
                     move = maxValue(move, b, -999999999,999999999, 0, i, System.currentTimeMillis() - start);
                 }
                 b.AIMove(move[0], move[1]);
@@ -40,7 +39,7 @@ public class Main {
         }
         int bestValue = -999999999;
         int[] newMove = new int[2], childMove = new int[2];
-        Board child = null;
+        Board child = new Board(b);
 
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
@@ -48,13 +47,13 @@ public class Main {
                     childMove[0] = i; childMove[1] = j;
                     child = new Board(b);
                     child.algoMove(i, j, 'X');
-                    if (child.evaluate('X') > b.evaluate('X')) {
+                    if (child.eval('X') > b.eval('X')) {
                         long newTime = System.currentTimeMillis() - time;
                         newMove = minValue(childMove, child, alpha, beta, depth + 1, maxDepth, newTime);
                         child.algoMove(newMove[0], newMove[1], 'X');
-                        bestValue = Math.max(bestValue, child.evaluate('X'));
+                        bestValue = Math.max(bestValue, child.eval('X'));
                         alpha = Math.max(alpha, bestValue);
-                        if (beta <= alpha || newTime < 0){
+                        if (beta <= alpha || newTime <= 0) {
                             return newMove;
                         }
 
@@ -66,12 +65,12 @@ public class Main {
     }
 
     public static int[] minValue(int[] move, Board b, int alpha, int beta, int depth, int maxDepth, long time) {
-        if (depth == maxDepth || b.isTerminalState()) {
+        if (depth == maxDepth || b.isTerminalState())  {
             return move;
         }
         int bestValue = 999999999;
         int[] newMove = new int[2], childMove = new int[2];
-        Board child = null;
+        Board child = new Board(b);
 
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
@@ -79,13 +78,13 @@ public class Main {
                     childMove[0] = i; childMove[1] = j;
                     child = new Board(b);
                     child.algoMove(i, j, 'O');
-                    if (child.evaluate('O') > b.evaluate('O')) {
+                    if (child.eval('O') > b.eval('O')) {
                         long newTime = System.currentTimeMillis() - time;
                         newMove = maxValue(childMove, child, alpha, beta, depth + 1, maxDepth, newTime);
                         child.algoMove(newMove[0], newMove[1], 'O');
-                        bestValue = Math.min(bestValue, child.evaluate('O'));
+                        bestValue = Math.min(bestValue, child.eval('O'));
                         beta = Math.min(beta, bestValue);
-                        if (beta <= alpha){
+                        if (beta <= alpha || newTime <= 0) {
                             return newMove;
                         }
 
